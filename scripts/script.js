5ff4b62ac5a2dbb871c57180dfdb0d9f787e2613
DianacraftGaming <lifehack4you.diana@gmail.com>
2021-11-06 20:24:39 +0200

    Looks  like a nice working base. Let it be 1.0 alpha

--- scripts/script.js
+++ scripts/script.js
@@ -1,89 +1,70 @@
-let currentTab = 2;
+let currentTab;
 let tabName1 = "<img src=\"images/rules.png\" class=\"btnImg\">"
 let tabName2 = "<img src=\"images/game.png\" class=\"btnImg\">"
 let tabName3 = "<img src=\"images/settings.png\" class=\"btnImg\">"
-let sizeX = 10;
-let sizeY = 10;
+let sizeX = 11;
+let sizeY = 11;
 
 
-let field = document.getElementById('field');
-let cellWidth = field.clientWidth / sizeX - 4;
-let cellHeight = field.clientHeight / sizeY - 4;
-for(let i = 0; i<sizeX; i++){
-	for(let i = 0; i<sizeX; i++){
-		const square = document.createElement('div');
-    	square.classList.add('cell');
 
-    	square.style.width = `${cellWidth}px`;
-    	square.style.height = `${cellHeight}px`;
-
-    	//square.addEventListener('mouseover', () => setColor(square))
-    	//square.addEventListener('mouseleave', () => removeColor(square))
-
-
-    	field.append(square)
-	}
+function onMouseClick(square){
+	square.classList.add('open')
 }
 
+function onMouseDblClick(square){
+	square.classList.remove('open')
+}
 
 function setting(){
-	let wrapper = document.getElementById('wrap');
+	currentTab = 2;
+	show_tab(currentTab);
 
 	window.battom1 = document.getElementById('bat_1');
 	window.battom2 = document.getElementById('bat_2');
 	window.battom3 = document.getElementById('bat_3');
 
+	battom1.addEventListener('click', () => show_tab(1));
+	battom2.addEventListener('click', () => show_tab(2));
+	battom3.addEventListener('click', () => show_tab(3));
+
 	battom1.innerHTML = tabName1;
 	battom2.innerHTML = tabName2;
 	battom3.innerHTML = tabName3;
 
-	window.taba1 = document.getElementById('tab_1');
-	window.taba2 = document.getElementById('tab_2');
-	window.taba3 = document.getElementById('tab_3');
+	let field = document.getElementById('field');
+	let cellWidth = field.clientWidth / sizeX - 4; //TODO: count the '4' via cell/square
+	let cellHeight = field.clientHeight / sizeY - 4;
+	for(let i = 0; i<sizeX; i++){
+		for(let i = 0; i<sizeX; i++){
+			const square = document.createElement('div');
+    		square.classList.add('cell');
 
-	let newWidth = battom1.clientWidth + battom2.clientWidth + battom3.clientWidth;
-	newWidth += 6;
-	
-	wrapper.style.width = newWidth + 'px';
-	taba1.style.width = newWidth + 'px';
-	taba2.style.width = newWidth + 'px';
-	taba3.style.width = newWidth + 'px';
+	    	square.style.width = `${cellWidth}px`;
+    		square.style.height = `${cellHeight}px`;
+
+			square.addEventListener('click', () => onMouseClick(square))
+			square.addEventListener('dblclick', () => onMouseDblClick(square))
+
+    		field.append(square)
+		}
+	}
+}
+
+function changeActivity(num, state){
+	let button = document.getElementById('bat_'+num);
+	let tab = document.getElementById('tab_'+num);
+
+	if(state){
+		button.classList.add('active');
+		tab.classList.add('active');
+	} else {
+		button.classList.remove('active');
+		tab.classList.remove('active');
+	}
 }
 
 function show_tab(num) {
-	if(num == 1 && num != currentTab){
-		taba1.style.display = 'block';
-		battom1.style.border = '3px solid #8a7254';
-
-		battom2.style.border = '3px outset #DEB887';
-		battom3.style.border = '3px outset #DEB887';
-
-		taba2.style.display = 'none';
-		taba3.style.display = 'none';
-		currentTab = num;
-	}
-
-	if(num == 2 && num != currentTab){
-		taba2.style.display = 'block';
-		battom2.style.border = '3px solid #8a7254';
-
-		battom1.style.border = '3px outset #DEB887';
-		battom3.style.border = '3px outset #DEB887';
-
-		taba1.style.display = 'none';
-		taba3.style.display = 'none';
-		currentTab = num;
-	}
-
-	if(num == 3 && num != currentTab){
-		taba3.style.display = 'block';
-		battom3.style.border = '3px solid #8a7254';
-
-		battom2.style.border = '3px outset #DEB887';
-		battom1.style.border = '3px outset #DEB887';
-
-		taba2.style.display = 'none';
-		taba1.style.display = 'none';
-		currentTab = num;
-	}
+	changeActivity(currentTab, false);
+	changeActivity(num, true);
+	currentTab = num;
 }
